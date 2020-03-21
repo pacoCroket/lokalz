686e0ca7319365fc44114aa15f020a6590305bbe
Paco Croket <croketproduction@gmail.com>
2020-03-21 21:11:18 +0100

    auth logic

--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", event => {});
+document.addEventListener("DOMContentLoaded", event => {
+  // const app = firebase.app();
+  const db = firebase.firestore();
+
+  const shops = db.collection("shops");
+
+  function getUserDoc() {
+    const currentUser = firebase.auth().currentUser;
+    users
+      .doc(currentUser.uid)
+      .get()
+      .then(doc => {});
+  }
+});
