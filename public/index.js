e05222099813c9d9f0cb373572ae26bb0dcf4428
Civan Erbay <Civan.Erbay@questback.com>
2020-03-21 22:53:17 +0100

    smooth animation

--- public/index.js
+++ public/index.js
@@ -1,1 +1,18 @@
-document.addEventListener("DOMContentLoaded", event => {});
+document.addEventListener("DOMContentLoaded", event => {
+    $("#event_toggle").click(function() {
+        var active = $(".fold.active");
+        console.log("blablabla");
+        // if there is visible fold element on page (user already clicked at least once on link)
+        if (active.length) {
+            $(".fold.active").animate({ height: "0" }, 1000, function() {
+                // this happens after above animations are complete
+                $(this).removeClass("active");
+            });
+            // clicking for the first time
+        } else {
+            $("#hide_open")
+                .addClass("active")
+                .animate({ height: "250px" }, 1000);
+        }
+    });
+});
