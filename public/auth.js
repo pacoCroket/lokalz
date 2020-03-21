2ca488a2d262dfadc63f8a5158dba770b527a707
Paco Croket <croketproduction@gmail.com>
2020-03-21 21:28:53 +0100

    getShops

--- public/auth.js
+++ public/auth.js
@@ -1,61 +1,60 @@
 document.addEventListener("DOMContentLoaded", event => {
-    const users = db.collection("users");
+  const usersRef = db.collection("users");
 
-    function signOut() {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          dispatch({ type: "SIGNOUT_SUCCESS" });
-        })
-        .catch(err => {
-          dispatch({ type: "SIGNOUT_ERROR", err });
+  function signOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: "SIGNOUT_SUCCESS" });
+      })
+      .catch(err => {
+        dispatch({ type: "SIGNOUT_ERROR", err });
+      });
+  }
+
+  function signUp(newUser) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then(resp => {
+        return usersRef.doc(resp.user.uid).set({
+          firstName: newUser.firstName,
+          lastName: newUser.lastName
         });
-    }
-  
-    function signUp(newUser) {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(newUser.email, newUser.password)
-        .then(resp => {
-          return users.doc(resp.user.uid).set({
-            firstName: newUser.firstName,
-            lastName: newUser.lastName
-          });
-        })
-        .then(() => {
-          dispatch({ type: "SIGNUP_SUCCESS" });
-        })
-        .catch(err => {
-          dispatch({ type: "SIGNUP_ERROR", err });
-        });
-    }
-  
-    function emailAndPasswordLogin(credentials) {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(credentials.email, credentials.password)
-        .then(() => {
-          dispatch({ type: "LOGIN_SUCCESS" });
-        })
-        .catch(err => {
-          dispatch({ type: "LOGIN_ERROR", err });
-        });
-    }
-  
-    // not used
-    function googleLogin() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-  
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(res => {
-          const user = res.user;
-          document.write("hello " + user.displayName);
-          console.log(user);
-        })
-        .catch(err => console.log(err));
-    }
-  
-}
+      })
+      .then(() => {
+        dispatch({ type: "SIGNUP_SUCCESS" });
+      })
+      .catch(err => {
+        dispatch({ type: "SIGNUP_ERROR", err });
+      });
+  }
+
+  function emailAndPasswordLogin(credentials) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({ type: "LOGIN_SUCCESS" });
+      })
+      .catch(err => {
+        dispatch({ type: "LOGIN_ERROR", err });
+      });
+  }
+
+  // not used
+  function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(res => {
+        const user = res.user;
+        document.write("hello " + user.displayName);
+        console.log(user);
+      })
+      .catch(err => console.log(err));
+  }
+});
--- public/index.js
+++ public/index.js
@@ -2,13 +2,30 @@ document.addEventListener("DOMContentLoaded", event => {
   // const app = firebase.app();
   const db = firebase.firestore();
 
-  const shops = db.collection("shops");
+  const usersRef = db.collection("users");
+  const shopsRef = db.collection("shops");
+  const currentUser = firebase.auth().currentUser;
 
   function getUserDoc() {
-    const currentUser = firebase.auth().currentUser;
-    users
+    if (!currentUser) return;
+    usersRef
       .doc(currentUser.uid)
       .get()
       .then(doc => {});
+  }
+
+  function getShops(query) {
+    shopsRef
+      .where("location", "==", true)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+        });
+      })
+      .then(err => {
+        console.log(err);
+      });
   }
 });
