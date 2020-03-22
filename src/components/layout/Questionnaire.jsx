e635f89b05eda429607570ead5b1a34c4994d05f
Paco Croket <croketproduction@gmail.com>
2020-03-22 22:21:46 +0100

    Revert "backend update"
    This reverts commit dc3dca355096112c7f6e90eba9e54dcf6f632a7e.

--- src/components/layout/Questionnaire.jsx
+++ src/components/layout/Questionnaire.jsx
@@ -194,25 +194,3 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(null, mapDispatchToProps)(Questionnaire);
-
-function threeOptionQuestion((props) {
-    return (
-      <div className="Comment">
-        <div className="UserInfo">
-          <img className="Avatar"
-            src={props.author.avatarUrl}
-            alt={props.author.name}
-          />
-          <div className="UserInfo-name">
-            {props.author.name}
-          </div>
-        </div>
-        <div className="Comment-text">
-          {props.text}
-        </div>
-        <div className="Comment-date">
-          {formatDate(props.date)}
-        </div>
-      </div>
-    );
-  }
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,15 +4,15 @@ import "firebase/storage";
 import "firebase/database";
 import "firebase/auth";
 
-const fbConfig = {
+export const fbConfig = {
   apiKey: "AIzaSyAoM6QHagKb1zwYwDjUC0_BTDZsn_6NrRY",
   authDomain: "lokalz.firebaseapp.com",
   databaseURL: "https://lokalz.firebaseio.com",
   projectId: "lokalz",
   storageBucket: "lokalz.appspot.com",
   messagingSenderId: "238230218877",
-  appId: "1:238230218877:web:4a0e0d458cb93f9deaa914",
-  measurementId: "G-S46BEH6589"
+  appId: "1:238230218877:web:c5a3119efeadda64eaa914",
+  measurementId: "G-98Z6QSYKHY"
 };
 
 // initialize Firebase with config
