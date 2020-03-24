54975b788cdbd1ec7f84744e8eaa3f74850a9f69
navic1992 <civan.erbay@web.de>
2020-03-24 19:19:56 +0100

    landing template 2 finish

--- src/components/layout/HomePage.jsx
+++ src/components/layout/HomePage.jsx
@@ -17,71 +17,41 @@ class HomePage extends Component {
 
         return (
             <div className="landing">
-                <div className="row section1" id="home">
-                    <div className="cath col-md-3 p-0 mr-5">
-                        {/* <Image
-                            className="ml-auto"
-                            src="/assets/images/CologneCathedralSilhouette.svg"
-                            rounded
-                        /> */}
-                        <h3 className="sm_title text-right border-left-0 ">
-                            {brandString}
-                        </h3>
+                <div className="row section1 frontBackground" id="home">
+                    <div className="cath col-md-2 p-0">
+                       
+                      
                     </div>
-                    {/* <div className="col-md-1"></div> */}
-                    <div className="col-md center px-5">
+                   
+                    <div className="col-md center">
                         <h2 className="front_header">
-                            <span className="logo_pos">Support</span> your{" "}
-                            <br /> local Shop.
-                            {/* <span
-                                style={{
-                                    color: "var(--accent-secondary-color)"
-                                }}
-                            >
-                               
-                            </span> */}
+                            {/* <span className="logo_pos"> </span>*/} Lokal <br/> nachhaltig <br/> progressiv 
+                            
+                          
                         </h2>
                         <p className="front_text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.
+                        Die LOKALZ-Initiative ist eine Herzensangelegenheit, welche aus den Herausforderungen angesichts der weltweit wütenden Corona-Krise heraus entstanden ist. Wir haben es uns zum Ziel gemacht, den lokalen Einzelhandel Kölns durch eine gemeinsame Online-Plattform für die Kölner*innen sichtbar zu machen, um so einen Gegenpol zu den marktführenden Online-Versandhäusern schaffen. 
                         </p>
                     </div>
 
                 
-                <div className="cath2 col-md-3 span4 p-0">
-                    <Image
-                            className="ml-auto"
-                            src="/assets/images/CologneCathedralSilhouette.svg"
-                            rounded
-                        /></div>
+                <div className="cath2 col-md-5 span4 p-0">
+     
+                        </div>
                 </div>
                 <div className="diagonal"></div>
-                <div className="diagonal2"></div>
+                {/* <div className="diagonal2"></div> */}
                 <div className="row section2" id="about">
-                    <div className="col-md-3 circle_pos"></div>
-
+                    <div className="col-md-3 pl-0">
+                  
+                        </div>  
                     <div className="col-md-6 circle_pos">
                         {/* IMAGE1 */}
                         <div className="d-flex justify-content-end align-items-center img_wrap">
-                            <Image
-                                className="pic"
-                                
-                                src="/assets/images/commerce.svg"
-                                rounded
-                            />
-                            <span className="text_pos text-center">
-                                <h4 className="pic_title text-left">
+                            <span className="text_pos">
+                                <h4 className="pic_title text-center">
                                     Lokal
-                                    <span
-                                        style={{
-                                            color: "var(--accent-secondary-color)"
-                                        }}
-                                    >
-                                        !
-                                    </span>
+                                
                                 </h4>
                                 <p className="pic_text text-center">
                                     Lorem ipsum dolor sit amet, consectetur
@@ -93,24 +63,11 @@ class HomePage extends Component {
                         </div>
                         {/* IMAGE2 */}
                         <div className="d-flex justify-content-end align-items-center flex-row-reverse img_wrap">
-                            <Image
-                                className="pic"
-                                // style={{ backgroundColor: "darkred" }}
-                                src="/assets/images/growth.svg"
-                                rounded
-                            />
+                     
                             <span className="text_pos text-center">
-                                <h4 className="pic_title text-right">
+                                <h4 className="pic_title text-center">
                                     Solidarisch
-                                    <span
-                                        style={{
-                                            color: "var(--accent-secondary-color)"
-                                        }}
-                                    >
-                                        !
-                                    </span>
                                 </h4>
-
                                 <p className="pic_text text-center">
                                     Lorem ipsum dolor sit amet, consectetur
                                     adipiscing elit, sed do eiusmod tempor
@@ -122,22 +79,11 @@ class HomePage extends Component {
 
                         {/* IMAGE3 */}
                         <div className="d-flex justify-content-end align-items-center">
-                            <Image
-                                className="pic"
-                                // style={{ backgroundColor: "darkred" }}
-                                src="/assets/images/green.svg"
-                                rounded
-                            />
+                         
                             <span className="text_pos">
                                 <h4 className="pic_title text-center">
                                     Umweltbewusst
-                                    <span
-                                        style={{
-                                            color: "var(--accent-secondary-color)"
-                                        }}
-                                    >
-                                        !
-                                    </span>
+                              
                                 </h4>
                                 <p className="pic_text text-center">
                                     Lorem ipsum dolor sit amet, consectetur
