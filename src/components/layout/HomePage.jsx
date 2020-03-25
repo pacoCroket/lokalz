45e5ede92c84ec67ecd99877b00941cbd84e4328
Paco Croket <croketproduction@gmail.com>
2020-03-25 21:20:44 +0100

    responsive

--- src/components/layout/HomePage.jsx
+++ src/components/layout/HomePage.jsx
@@ -7,97 +7,90 @@ import { Row, Col } from "react-bootstrap";
 import Subscribe from "./Subscribe";
 
 class HomePage extends Component {
-    render() {
-        const { notifications } = this.props;
+  render() {
+    const { notifications } = this.props;
 
-        const brandString = (
-            <>
-                <b>Lokal</b>Zone.
-            </>
-        );
+    const brandString = (
+      <>
+        <b>Lokal</b>Zone.
+      </>
+    );
 
-        return (
-            <div className="landing">
-                <Row className="section1 frontBackground" id="home">
-                    <Col></Col>
-                    <Col md={5}>
-                        <div className="statement">
-                            <h2>
-                                {/* Online-Plattform zur Stärkung des lokalen Einzelhandels in Köln. */}
-                                Lokal. <br />
-                                Nachhaltig. <br /> Progressiv.
-                            </h2>
-                        </div>
-                    </Col>
+    const newsletterElement = (
+      <div className="newsletter">
+        <h3 className="mr-auto">Newsletter</h3>
 
-                    <Col> </Col>
-                    <Col md={5}>
-                      <div className="d-flex flex-column justify-content-center align-items-center">
-                    <p className="">
-                                Wir machen euch sichtbar. <br/> Während und
-                                nach der Krise. <br/> <b>kostenfrei & gemeinnützig</b> 
-                            </p>
-                            
-                        <div id="container" className="">
-                       
-                            <h3>Newsletter</h3>
-                            
-                            <form action="" className="text-center">
-                                <input
-                                    type="email"
-                                    placeholder="deine@email.de"
-                                    required
-                                ></input>
-                                <br />
-                                <button>Subscribe</button>
-                            </form>
-                        </div>
-                        </div>
-                    </Col>
-                    <Col> </Col>
-                </Row>
+        <form action="" className="text-center d-flex px-3">
+          <input type="email" placeholder="deine@email.de" required></input>
+          {/* <br /> */}
+          <button>Subscribe</button>
+        </form>
+      </div>
+    );
 
-                <Row className="section2" id="about">
-                
-                    <Col md={8}>
-                        <h2 className="">Die Idee</h2>
-                        <p>
-                            Wir verstehen uns als Plattform die auf der einen
-                            Seite Kunden die Möglichkeit bietet ihren Kölner
-                            Lieblingsshop auch angesichts der durch das
-                            Corona-Virus erschwerten Bedingungen zu
-                            unterstützen. Auf der anderen Seite wollen wir
-                            lokale Einzelhändler in ganz Köln die Möglichkeit
-                            bieten sich online zu präsentieren und direkten
-                            Kontakt zu den Menschen in ihrer Umgebung
-                            herzustellen. Jeder lokale Einzelhändler benötigt
-                            eine Online-Präsenz mit Produktübersicht,
-                            Zahlungsabwicklung und Logisitik. Weil wir alle
-                            derzeit aufgefordert sind uns so wenig wie moeglich
-                            draussen aufzuhalten wird auch das Einkaufen
-                            erschwert und der Rueckgriff auf grosse Versandshops
-                            attraktiver. Weil dies den lokalen Handel schwaecht,
-                            der gerade jetzt moeglichst viel Unterstuetzung
-                            braucht wollen wir mit dieser Plattform eine neue
-                            Art der Kommunikation zwischen Kunden und lokalem
-                            Einzelhandel gewährleistet, sodass nicht jeder
-                            Onlinekauf bei den Marktgiganten erfolgen muss. So
-                            wollen wir gemeinsam mit unseren Nachbar*innen und
-                            den Geschaeften in unserer direkten Umgebung die
-                            Corona-Krise meistern und ein Gegengewicht zu den
-                            marktführenden Online-Versandhändlern schaffen,
-                            deren Produkte im Internet professionell vermarktet,
-                            dreimal um die Welt verschickt werden und in Zeiten
-                            eines Shutdowns einen enormen Wettbewerbsvorteil
-                            gegenüber lokalen Fachgeschäften haben, die derzeit
-                            der Gefahr einer drohenden Insolvenz ausgesetzt
-                            sind.
-                        </p>
-                    </Col>
-                    <Col> <img src="/assets/images/strangeSymbol.svg" alt="" className="shapeSize" srcset=""/> </Col>
-                </Row>
+    return (
+      <div className="landing">
+        <Row className="section1 frontBackground" id="home">
+          <Col></Col>
+          <Col md={5} className="px-4">
+            <div className="statement">
+              <h2>
+                {/* Online-Plattform zur Stärkung des lokalen Einzelhandels in Köln. */}
+                Lokal. <br />
+                Nachhaltig. <br /> Progressiv.
+              </h2>
+            </div>
+          </Col>
 
-                {/* <div className="row section3" id="contact">
+          <Col md={4} className="px-4">
+            <div className="d-flex flex-column">
+              <p className="w-100">
+                Wir machen euch sichtbar. <br /> Während und nach der Krise. <br />{" "}
+                <b>kostenfrei & gemeinnützig</b>
+              </p>
+              {newsletterElement}
+            </div>
+          </Col>
+          <Col> </Col>
+        </Row>
+
+        <Row className="section2" id="about">
+          <Col></Col>
+          <Col md={8} className="px-4">
+            <h2>Die Idee</h2>
+            <p>
+              Wir verstehen uns als Plattform die auf der einen Seite Kunden die Möglichkeit bietet ihren
+              Kölner Lieblingsshop auch angesichts der durch das Corona-Virus erschwerten Bedingungen zu
+              unterstützen. Auf der anderen Seite wollen wir lokale Einzelhändler in ganz Köln die Möglichkeit
+              bieten sich online zu präsentieren und direkten Kontakt zu den Menschen in ihrer Umgebung
+              herzustellen.
+            </p>
+            <p>
+              Jeder lokale Einzelhändler benötigt eine Online-Präsenz mit Produktübersicht, Zahlungsabwicklung
+              und Logisitik. Weil wir alle derzeit aufgefordert sind uns so wenig wie moeglich draussen
+              aufzuhalten wird auch das Einkaufen erschwert und der Rueckgriff auf grosse Versandshops
+              attraktiver. Weil dies den lokalen Handel schwaecht, der gerade jetzt moeglichst viel
+              Unterstuetzung braucht wollen wir mit dieser Plattform eine neue Art der Kommunikation zwischen
+              Kunden und lokalem Einzelhandel gewährleistet, sodass nicht jeder Onlinekauf bei den
+              Marktgiganten erfolgen muss.
+            </p>
+            <p>
+              So wollen wir gemeinsam mit unseren Nachbar*innen und den Geschaeften in unserer direkten
+              Umgebung die Corona-Krise meistern und ein Gegengewicht zu den marktführenden
+              Online-Versandhändlern schaffen, deren Produkte im Internet professionell vermarktet, dreimal um
+              die Welt verschickt werden und in Zeiten eines Shutdowns einen enormen Wettbewerbsvorteil
+              gegenüber lokalen Fachgeschäften haben, die derzeit der Gefahr einer drohenden Insolvenz
+              ausgesetzt sind.
+            </p>
+            <div className="mx-auto w-75 p-5">{newsletterElement}</div>
+          </Col>
+          <Col>
+            {" "}
+            <img src="/assets/images/strangeSymbol.svg" alt="" className="shapeSize" srcset="" />{" "}
+          </Col>
+        </Row>
+
+        {/* <div className="row section3" id="contact">
                     <div className="col-md-1"></div>
                     <div className="col-md-2"></div>
                     <div className="col-md-1"></div>
@@ -108,26 +101,24 @@ class HomePage extends Component {
                     <div className="col-md-1"></div>
                     <div className="col-md-2"></div>
                 </div> */}
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    const { notifications } = state.firestore.ordered;
-    return {
-        notifications,
-        auth: state.firebase.auth
-    };
+  const { notifications } = state.firestore.ordered;
+  return {
+    notifications,
+    auth: state.firebase.auth
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {};
+  return {};
 };
 
 export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    firestoreConnect([
-        { collection: "notifications", limit: 5, orderBy: ["time", "desc"] }
-    ])
+  connect(mapStateToProps, mapDispatchToProps),
+  firestoreConnect([{ collection: "notifications", limit: 5, orderBy: ["time", "desc"] }])
 )(HomePage);
