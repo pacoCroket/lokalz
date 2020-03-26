5cae7aad5a13aebe8d984574f7eb1f4c2ad563c8
Paco Croket <croketproduction@gmail.com>
2020-03-27 00:23:20 +0100

    justify

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,29 +2,29 @@ import React, { Component } from "react";
 import "./../../style/footer.css";
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <footer className="footer-area mt-auto" id="footer-area">
-                <div className="container">
-                    <div className="site-logo text-center">
-                        <div className="social text-center py-2">
-                            <h5 className="text-uppercase">Follow us</h5>
-                            <a href="" className="">
-                                <i className="fab fa-twitter-square"></i>
-                            </a>
-                            <a href="" className="">
-                                <i className="fab fa-facebook-square"></i>
-                            </a>
-                            <a href="" className="">
-                                <i className="fab fa-instagram"></i>
-                            </a>
-                            <a href="" className="">
-                                <i className="fab fa-github"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        );
-    }
+  render() {
+    return (
+      <footer className="footer-area mt-auto" id="footer-area">
+        <div className="container">
+          <div className="site-logo text-center">
+            <div className="social text-center">
+              <h5 className="text-uppercase">Follow us</h5>
+              <a href="" className="">
+                <i className="fab fa-twitter-square"></i>
+              </a>
+              <a href="" className="">
+                <i className="fab fa-facebook-square"></i>
+              </a>
+              <a href="" className="">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="" className="">
+                <i className="fab fa-github"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
